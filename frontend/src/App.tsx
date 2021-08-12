946de825237c0561c8d259c6de5010dafbd2bb50
Jason Cramer <jason@Jasons-MacBook-Pro.local>
2021-08-11 20:42:56 -0400

    Added router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import AuthenticationProvider from '@contexts/AuthContext';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+ } from 'react-router-dom';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <AuthenticationProvider>
-        
+        <Router>
+          <Switch>
+            <Route exact path="/"></Route>
+          </Switch>
+        </Router>
       </AuthenticationProvider>
     </div>
   );
