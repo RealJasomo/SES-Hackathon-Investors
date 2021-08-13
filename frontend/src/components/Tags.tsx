6f99274b0b206a94e235ffb818f61d3fcc2ed6c2
Jason Cramer <jason@Jasons-MacBook-Pro.local>
2021-08-12 20:57:52 -0400

    changed to # + limit to 5

--- frontend/src/components/Tags.tsx
+++ frontend/src/components/Tags.tsx
@@ -10,6 +10,6 @@ export default function Tags(props: ITagsProps){
     return (
     <div className={styles.tagsContainer}>
         <p>Tags:</p>
-        {props.tags.map((tag, idx) => <div className={styles.tag} key={idx}>{tag}</div>)}
+        {props.tags.slice(0, 5).map((tag, idx) => <div className={styles.tag} key={idx}>{tag}</div>)}
     </div>);
 }
