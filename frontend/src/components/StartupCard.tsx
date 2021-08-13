658684c249660a0e406b7340a12a550d41118d10
Jason Cramer <jason@Jasons-MacBook-Pro.local>
2021-08-12 20:07:52 -0400

    made startup card

--- frontend/src/components/StartupCard.tsx
+++ frontend/src/components/StartupCard.tsx
@@ -3,6 +3,7 @@ import Startup from '@interfaces/Startup';
 import Avatar from '@material-ui/core/Avatar';
 
 import styles from './StartupCard.module.scss';
+import LinearProgress from '@material-ui/core/LinearProgress';
 
 interface IStartupCardProps{
     startup: Startup
@@ -11,8 +12,11 @@ interface IStartupCardProps{
 export default function StartupCard(props: IStartupCardProps){
     return (
     <div className={styles.startupCard}>
-        <h1><Avatar src={props.startup.logo} alt="startup logo"/>{props.startup.name}</h1>
-        <p>{props.startup.description}</p>
-        <p> raised ${props.startup.amountInvested.toLocaleString()} / goal $ {props.startup.goal.toLocaleString()}</p>
+        <h1 className={styles.header}><Avatar className={styles.avatar} src={props.startup.logo} alt="startup logo"/>{props.startup.name}</h1>
+        <p className={styles.description}>{props.startup.description}</p>
+        <div className={styles.goal}>
+            <p> raised ${props.startup.amountInvested.toLocaleString()} / goal $ {props.startup.goal.toLocaleString()}</p>
+            <LinearProgress variant="determinate" value={Math.round(100*(props.startup.amountInvested / props.startup.goal))}/>
+        </div>
     </div>)
 }
