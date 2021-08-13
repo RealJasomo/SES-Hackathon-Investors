b3cc8fa875625b82ecce3df7526336a6fb38fa7e
Jason Cramer <jason@Jasons-MacBook-Pro.local>
2021-08-12 23:14:39 -0400

    made better homescreen

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import withAuthentication from '@hoc/withAuthentication';
 
+import MeetTheTeam from '@res/meetTheTeam.png';
+import LandingHeader from '@res/landingHeader.png'
 import styles from './HomePage.module.scss';
 
 function HomePage(){
 
     return (
         <div className={styles.home}>
+            <img src={LandingHeader} alt="header"/>
+            <img src={MeetTheTeam} alt="meet the team"/>
         </div>);
 }
 
