5367aba88b59f725baf6be474456db5aaea25faa
Jason Cramer <jason@Jasons-MacBook-Pro.local>
2021-08-13 00:52:50 -0400

    Added locations to user

--- frontend/src/components/UserCard.tsx
+++ frontend/src/components/UserCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Avatar  from '@material-ui/core/Avatar';
 import Tags from '@components/Tags';
+import Country from './Country';
 
 import { capitalize } from '@util/utils';
 import User from '@interfaces/User';
@@ -17,6 +18,7 @@ export default function UserCard(props: IUserCardProps) {
         <Avatar className={styles.avatar} src={props.user?.profilePhoto} alt="profile photo" />
         <p className={styles.name}>Name: {capitalize(props.user?.firstName ?? '')} {capitalize(props.user?.lastName ?? '')}</p>
         <p className={styles.bio}>Bio: {props.user?.bio}</p>
+        <Country country={props.user?.country ?? ''} state={props.user?.state}/>
         <Tags tags={props.user?.tags ?? []}/>
     </div>);
 }
